cb04113608a065eae3258c1feb93f05aafd94b27
Clau905 <clau905@icloud.com>
2023-04-27 11:23:04 -0300

    configurando firebase

--- public/config/firebase.js
+++ public/config/firebase.js
@@ -1,6 +1,7 @@
-// Import the functions you need from the SDKs you neefi
+// Import the functions you need from the SDKs you need
+
+
 import { initializeApp } from "firebase/app";
-import { getFireStore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,13 +12,13 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
 console.log('estoy config firebase ',firebaseConfig)
+const app = initializeApp(firebaseConfig);
+
 
 
 const db = getFireStore(app);
